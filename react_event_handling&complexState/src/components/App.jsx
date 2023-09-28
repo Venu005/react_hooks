55df52358dfd387d5f00038729647a5c6b4db037
Venusai <118623801+Venu005@users.noreply.github.com>
2023-09-28 10:35:05 +0530

    Update App.jsx
    Using ejs 6 spread(....objName) to update the fields in the form

--- react_event_handling&complexState/src/components/App.jsx
+++ react_event_handling&complexState/src/components/App.jsx
@@ -10,26 +10,36 @@ function App() {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+    // setContact(prevValue => {
+    //   if (name === "fName") {
+    //     return {
+    //       fName: value,
+    //       lName: prevValue.lName,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "lName") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: value,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "email") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: prevValue.lName,
+    //       email: value
+    //     };
+    //   }
+    // });
+    // USING SPREAD OPERATOR FROM EJS 6
+    setContact(preValue=>{
+      return{
+        // so in this spread operator when an array [name] is used aand given the value,.it updates the contents of already existing name
+        // i.e [fName,lName,email] but if just a key value is gibven like name instead of an array,it's like adding a new key value pair to the exising object preValue
+        // this EJS 6 tool spread(...objName) can come a lot handy __ notes taken from Angela 
+        ...preValue,
+        [name] : value,
+    };
     });
   }
 
